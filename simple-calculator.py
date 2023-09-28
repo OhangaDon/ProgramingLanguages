c40216b4c51a301ff0ae53e6048c1fa43c24abf0
OhangaDon <117031722+OhangaDon@users.noreply.github.com>
2023-09-28 22:04:03 +0300

    Add files via upload

---
+++ simple-calculator.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+sum = (num1) + (num2)
+product = (num1) * (num2)
+dividend = (num1) / (num2)
+modulus = (num1) % (num2)
+
+print(f"The sum of the two numbers is {sum}")
+print(f"The product of the two numbers is {product}")
+print(f"The dividend of the two number is {dividend}")
+print(f"The modulus of the two numbers is {modulus}")
